2d4bcddec88662d154a5ce69b7befb1326a9a8e8
Alessio Bolognesi <abolognesi00@gmail.com>
2021-07-03 12:29:17 +0200

    Minor Imrovement

--- SnowballRun/Assets/Sckript/Player.cs
+++ SnowballRun/Assets/Sckript/Player.cs
@@ -1,49 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float forwardVelocity;
-    [SerializeField] private float lateralVelocity;
-    [SerializeField] private float jumpStrenght;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float jumpStrenght = 3;
+    [SerializeField] private float speedMove = 2;
 
-    // Update is called once per frame
+
     void Update()
     {
-        if (Input.GetKey(KeyCode.DownArrow))
+
+        if ( Input.GetKey(KeyCode.RightArrow) )
         {
-            transform.position += Vector3.left * forwardVelocity * Time.deltaTime;
+            transform.position += Vector3.right * speedMove * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        if ( Input.GetKey(KeyCode.LeftArrow) )
         {
-            transform.position += Vector3.right * forwardVelocity * Time.deltaTime;
+            transform.position += Vector3.left * speedMove * Time.deltaTime;
         }
            
-        if (Input.GetKey(KeyCode.Space))
+        if ( Input.GetKey(KeyCode.Space) )
         {
             transform.position += Vector3.up * jumpStrenght * Time.deltaTime;
         }
            
-        if (Input.GetKey(KeyCode.LeftAlt))
+        if ( Input.GetKey(KeyCode.LeftAlt) )
         {
             transform.position += Vector3.down * jumpStrenght * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position += Vector3.forward * lateralVelocity * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.position += Vector3.back * lateralVelocity * Time.deltaTime;
-        }
 
     }
+
 }
