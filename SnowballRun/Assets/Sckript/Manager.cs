157c838f6cf2fafc5b2d3059fa8c0e1961f4234e
Alessio Bolognesi <abolognesi00@gmail.com>
2021-07-03 11:29:26 +0200

    Added Game Script

---
+++ SnowballRun/Assets/Sckript/Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 dir;
+    public float GetSpeed()
+    {
+        return speed;
+    }
+    public Vector3 GetDirection()
+    {
+        return dir;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SnowballRun/Assets/Sckript/StreetMove.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StreetMove : MonoBehaviour
+{
+    Manager manager;
+    private MeshRenderer streetRender;
+    private Vector3 streetStats;
+    private Vector3 Offset;
+    private Material streetMat;
+
+    // chiamato prima di tutti gli "Start() { }" di ogni Script
+    void Awake()
+    {
+        streetRender = GetComponent<MeshRenderer>();
+        streetMat = streetRender.material;
+        manager = FindObjectOfType<Manager>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        streetStats = manager.GetSpeed() * manager.GetDirection() * Time.deltaTime;
+        Offset = streetMat.GetTextureOffset("_MainTex");
+        Offset += streetStats;
+        streetMat.SetTextureOffset("_MainTex", Offset);
+    }
+
+}
